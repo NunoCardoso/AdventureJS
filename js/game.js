a5c7061e5ebc94f8aebf9a17b4a419da08b37eac
Nuno Cardoso <nuno.cardoso@gmail.com>
2013-07-05 00:52:34 +0200

    walking right

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ function Game (options) {
 	this.o = options;
 	this.stage;
 	this.queue;
+	this.PC;
 
 	this.renderMenu = function () {
 		this.stage = new createjs.Stage("canvas");
@@ -10,27 +11,51 @@ function Game (options) {
 		var canvasY = $("#canvas").height();
 		var text = new createjs.Text(this.o.title, "20px Arial", "#ff0000");
 		text.x = 50; text.y=50;
-		var bitmap = new createjs.Bitmap(this.queue.getResult("background01"));
+		var bitmap = new createjs.Bitmap(this.queue.getResult(this.o.background));
 		bitmap.scaleX = canvasX / bitmap.image.width;
 		bitmap.scaleY = canvasY / bitmap.image.height;
 
+		var PCdata = {
+			images: [this.queue.getResult(this.o.PC.images)],
+			frames: this.o.PC.frames,
+			animations: this.o.PC.animations
+		};
+		var PCs = new createjs.SpriteSheet(PCdata);
+		this.PC = new createjs.BitmapAnimation(PCs);
+		this.PC.x=100;
+		this.PC.y=230;
+		this.PC.gotoAndPlay("walkright");
 		var wrapper = new createjs.Container();
 		wrapper.addChild(bitmap);
 		wrapper.addChild(text);
+		wrapper.addChild(this.PC);
 		this.stage.addChild(wrapper);
 
-		//Update stage will render next frame
-		this.stage.update();
+		createjs.Ticker.setFPS(40);
+		createjs.Ticker.addEventListener("tick", $.proxy(this.tick, this));
 	};
+
+
+	this.tick = function (event) {
+
+		this.PC.x += 2;
+		this.stage.update(event);
+	}
 
 
 	this.preloadImages = function () {
 		this.queue = new createjs.LoadQueue();
 		this.queue.addEventListener("complete", $.proxy(this.onPreloadComplete, this));
-		this.queue.loadManifest([
-		   	{id: "background01", 
-             src:this.o.background}
-	    ]);
+		var manifest = [];
+		for (var key in this.o.images) {
+			if (this.o.images.hasOwnProperty(key)) {
+				manifest.push({
+				   'id': key,
+				   'src' : this.o.images[key]
+				});
+			}
+		}
+		this.queue.loadManifest(manifest);
 	};
 
     this.onPreloadComplete = function () {
--- js/json.js
+++ js/json.js
@@ -1,5 +1,19 @@
 var gamejson = {
+    images: {
+		'background01': 'img/background-jungle.jpg',
+		'PC01' : 'img/guybrush.png'
+    },
     'title' : 'The compass',
     'description' : 'Can you build a compass?',
-    'background' : 'img/background-jungle.jpg'
+    'background' : 'background01',
+    'PC':{
+		'images': 'PC01',
+		'frames': {"regX": 0, "height": 150, "count": 18, "regY": 0, "width": 104},
+		'animations':{
+			'walkright':[0,5,'walkright',6],
+			'walkleft':[6,11,'walkleft',6],
+			'standright':12,
+			'standleft':13
+		}
+	}
 };
