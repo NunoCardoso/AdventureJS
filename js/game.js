41ae7c2b49c8d087134657368750a9d48d5c29d8
Nuno Cardoso <nuno.cardoso@gmail.com>
2013-07-06 00:36:29 +0200

    better structure

--- js/game.js
+++ js/game.js
@@ -3,75 +3,150 @@ function Game (options) {
 	this.o = options;
 	this.stage;
 	this.queue;
-	this.PC;
 	this.canvasX;
 	this.canvasY;
     this.clickedX;
 	this.clickedY;
-    this.attitude;
+	this.speed = 2;
+	this._ = {};
 
 	this.renderMenu = function () {
+
 		this.stage = new createjs.Stage("canvas");
 		this.canvasX = $("#canvas").width();
 		this.canvasY = $("#canvas").height();
-		var text = new createjs.Text(this.o.title, "20px Arial", "#ff0000");
-		text.x = 50; text.y=50;
-		var bitmap = new createjs.Bitmap(this.queue.getResult(this.o.background));
-		bitmap.scaleX = this.canvasX / bitmap.image.width;
-		bitmap.scaleY = this.canvasY / bitmap.image.height;
 
-		var PCdata = {
-			images: [this.queue.getResult(this.o.PC.images)],
-			frames: this.o.PC.frames,
-			animations: this.o.PC.animations
-		};
-		var PCs = new createjs.SpriteSheet(PCdata);
-		this.PC = new createjs.BitmapAnimation(PCs);
-		this.PC.x = 0;
-		this.PC.y = 230;
-		this.attitude = 'standright';
-		this.PC.gotoAndPlay(this.attitude);
+		this._.main = {};
 
-		var wrapper = new createjs.Container();
-		wrapper.addChild(bitmap, text, this.PC);
+		// Title
+		this._.main.title = new createjs.Text(this.o.main.title, "20px Arial", "#FFFFFF");
+		this._.main.title.textAlign = "center";
+		this._.main.title.textBaseline = "middle";
+		this._.main.title.x = this.canvasX / 2;
+		this._.main.title.y = 20;
+
+		// Author
+		this._.main.author = new createjs.Text(this.o.main.author, "17px Arial", "#CCCCCC");
+		this._.main.author.textAlign = "center";
+		this._.main.author.textBaseline = "middle";
+     	this._.main.author.x = this.canvasX / 2;
+		this._.main.author.y = 50;
+
+		// Description
+		this._.main.description = new createjs.Text(this.o.main.description, "14px Arial", "#FFFFFF");
+        this._.main.description.textAlign = "center";
+        this._.main.description.textBaseline = "middle";
+        this._.main.description.x = this.canvasX / 2;
+        this._.main.description.y = 100;
+
+		// Main Background
+		this._.main.background = new createjs.Bitmap(this.queue.getResult(this.o.main.background));
+		this._.main.background.scaleX = this.canvasX / this._.main.background.image.width;
+		this._.main.background.scaleY = this.canvasY / this._.main.background.image.height;
+
+		// start button
+		this._.main.startButton = new createjs.Shape();
+		this._.main.startButton.name = "startButton";
+		width = 200;
+		x = this.canvasX / 2 - width / 2;
+		y = 150;
+		height = 60;
+		round = 10;
+		this._.main.startButton.graphics.beginFill("red").drawRoundRect(x, y, width, height, round);
+		
+		// start button label
+		this._.main.startButtonLabel = new createjs.Text("start game!", "bold 24px Arial", "#FFFFFF");
+		this._.main.startButtonLabel.textAlign = "center";
+		this._.main.startButtonLabel.textBaseline = "middle";
+		this._.main.startButtonLabel.x = this.canvasX / 2;
+		this._.main.startButtonLabel.y = 150 + height / 2;
+
+		// objects
+
+		// PC
+		this._.PC = new createjs.BitmapAnimation(
+			new createjs.SpriteSheet({
+			    images: [this.queue.getResult(this.o.PC.images)],
+     			frames: this.o.PC.frames,
+		     	animations: this.o.PC.animations
+     		})
+        );
+		this._.PC.x = 0;
+		this._.PC.y = 230;
+		this._.PC.attitude = 'standright';
+		this._.PC.gotoAndPlay(this._.PC.attitude);
+		
+		// stage build		
+		var bottom = new createjs.Container();
+		var middle = new createjs.Container();
+
+		bottom.addChild(
+           this._.main.background,
+           this._.main.title,
+		   this._.main.author,
+		   this._.main.description
+    	);
+
+		middle.addChild(
+		   this._.main.startButton,
+		   this._.main.startButtonLabel,
+		   this._.PC
+	  	);
 
 		this.stage.autoClear = false;
-		this.stage.addChild(wrapper);
-		this.stage.addEventListener("click", $.proxy(this.onClick, this));
+		this.stage.addChild(bottom);
+		this.stage.addChild(middle);
+		
+		// listener on stage
+		// mousedown allows press event on objects above the stage to be captured
+		this.stage.addEventListener("mousedown", $.proxy(this.onStageMouseDown, this));
+		
+		// listener on button 
+		this._.main.startButton.addEventListener("press", $.proxy(this.onStartButtonPress, this));
 
+		this._.main.startButton.addEventListener("mouseover", $.proxy(this.onStartButtonMouseOver, this));
+
+		// ticker
 		createjs.Ticker.setFPS(40);
-		createjs.Ticker.addEventListener("tick", $.proxy(this.tick, this));
+		createjs.Ticker.addEventListener("tick", $.proxy(this.onTick, this));
 	};
 
-    this.onClick = function(e) {
+    this.onStartButtonPress = function (e) {
+		console.log("start button press!")
+	}
+
+    this.onStartButtonMouseOver = function (e) {
+		console.log("start button mouse over!")
+	}
+
+    this.onStageMouseDown = function(e) {
+		console.log("stage click!")
 		this.clickedX = e.stageX;
 		this.clickedY = e.stageY;
 	}
 
-	this.tick = function (event) {
-		this.speed = 2;
+	this.onTick = function (event) {
 		// attitudes
-		if (this.PC.x > this.clickedX && (this.PC.x - this.clickedX > this.speed)) {
-			this.attitude = "walkleft";
-		} else if (this.PC.x < this.clickedX  && (this.clickedX - this.PC.x > this.speed)) {
-            this.attitude = "walkright";
+		if (this._.PC.x > this.clickedX && (this._.PC.x - this.clickedX > this.speed)) {
+			this._.PC.attitude = "walkleft";
+		} else if (this._.PC.x < this.clickedX  && (this.clickedX - this._.PC.x > this.speed)) {
+            this._.PC.attitude = "walkright";
 		} else {
-			if (this.attitude == "walkleft") {
-				this.attitude = "standleft";
-			} else if (this.attitude == "walkright") {
-				this.attitude ="standright";
+			if (this._.PC.attitude == "walkleft") {
+				this._.PC.attitude = "standleft";
+			} else if (this._.PC.attitude == "walkright") {
+				this._.PC.attitude ="standright";
 			}
 		}
-
-		if (this.attitude == "walkleft") {
-			this.PC.x -= this.speed;			
-		} else if (this.attitude == "walkright") {
-			this.PC.x += this.speed;
+		if (this._.PC.attitude == "walkleft") {
+			this._.PC.x -= this.speed;			
+		} else if (this._.PC.attitude == "walkright") {
+			this._.PC.x += this.speed;
 		}
 		
 		// change attitude only if it is different
-		if (this.PC.currentAnimation != this.attitude) {
-			this.PC.gotoAndPlay(this.attitude);
+		if (this._.PC.currentAnimation != this._.PC.attitude) {
+			this._.PC.gotoAndPlay(this._.PC.attitude);
 		}
 		this.stage.update(event);
 	}
--- js/json.js
+++ js/json.js
@@ -1,19 +1,64 @@
 var gamejson = {
-    images: {
+    'images': {
 		'background01': 'img/background-jungle.jpg',
-		'PC01' : 'img/guybrush.png'
+		'PC01' : 'img/guybrush.png',
+        'winebottleinventory01' : 'img/wine-bottle-inventory.jpg',
+		'winebottle01' : 'img/wine-bottle.png'
     },
-    'title' : 'The compass',
-    'description' : 'Can you build a compass?',
-    'background' : 'background01',
-    'PC':{
+    'main': {
+		'title' : 'The compass',
+		'author' : 'teacher@school.com',
+		'description' : 'Can you build a compass?',
+		'background' : 'background01'
+	},
+    'PC': {
+        'id' : 'you01',
 		'images': 'PC01',
-		'frames': {"regX": 0, "height": 150, "count": 18, "regY": 0, "width": 104},
+		'frames': {'regX': 0, 'height': 150, 'count': 18, 'regY': 0, 'width': 104},
 		'animations':{
 			'walkright':[0,5,'walkright',6],
 			'walkleft':[6,11,'walkleft',6],
 			'standright':12,
 			'standleft':13
 		}
-	}
+	},
+	'objects':[
+       {
+       	'id': 'winebottle01',
+        'label': 'Wine bottle',
+	    'imageInInventory': 'winebottleinventory01',
+        'imageInScene' : 'winebottle01'
+	   }
+	],
+	'objectInteraction': [],
+	'scenes': [
+        {
+			"id": "scene01",
+			"background": "background01",
+			"preamble": "This is the first scene.",
+			"characters" : [
+                {
+					"who" : 'you01',
+					"position" : {
+						"x" : 0,
+						"y" : 230
+					}
+				}
+  			],
+	        'objects': [
+               {
+				   'id': 'winebottle01',
+				   'x' : 300,
+				   'y' : 300,
+				   'width' : 50,
+				   'height' : 50
+			   }
+            ]
+        } 
+   ],
+	"initialInventory": [],
+	"initialScene": {
+		"id": "scene01"
+	},
+    "dialogs": []
 };
